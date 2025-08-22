e1de30d728743346bd890ed4be7f482bdfb0ae8b
Ryan Tseng <ryan10328@gmail.com>
2025-08-22 23:00:45 +0800

    implement layout of create and index page

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,23 +2,28 @@ import React from 'react'
 import {Stack, useRouter} from "expo-router";
 import {Pressable} from "react-native";
 import {AntDesign} from "@expo/vector-icons";
+import {COLORS} from "../constants/colors";
 
 const RootLayout = () => {
     const router = useRouter();
     return (
-        <Stack>
+        <Stack screenOptions={{
+            headerStyle: {backgroundColor: COLORS.primary500},
+            headerTintColor: COLORS.secondary700,
+            headerTitleStyle: {color: COLORS.primary50},
+        }}>
             <Stack.Screen name="index" options={{
-                title: 'All Places',
+                title: 'Your favorite Places',
                 headerRight: ({tintColor}) => {
                     return (
-                        <Pressable onPress={() => router.replace("/create")}>
+                        <Pressable onPress={() => router.push("/create")}>
                             <AntDesign name="plus" color={tintColor} size={16}/>
                         </Pressable>
                     )
                 },
             }}/>
             <Stack.Screen name="create" options={{
-                title: 'Create Place',
+                title: 'Add Place',
             }}/>
         </Stack>
     );
